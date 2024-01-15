b2bacb743dfed1c77ad6c773ff5d39c19c6bd83b
WaileNaeem <133628747+WaileNaeem@users.noreply.github.com>
2024-01-15 14:21:31 +0500

    added arguments in useApi hooks

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -5,9 +5,10 @@ export default useApi = (apiFunc) => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  //args is used for passing parameters/arguments to api . rest operator ... is used to allow this function to take zero or more arguments . with this rest operator we get all the arguments and then put them in a single array
   const request = async (...args) => {
     setLoading(true);
-    const response = await apiFunc(...args);
+    const response = await apiFunc(...args); //args is used for passing parameters/arguments to api. now we can spread the array by calling api function. spread args
     setLoading(false);
 
     if (!response.ok) {
